ec618d1a11c1b59306546ef296f67164bb902bfb
Saswata Ghosh <42109896+Saswata6019@users.noreply.github.com>
2021-06-26 11:45:42 +0530

    Update IrisClassifier.py

--- IrisClassifier.py
+++ IrisClassifier.py
@@ -1,19 +1,14 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-# from subprocess import check_output  (running this will list the files in the input directory)
-# print(check_output(["ls", "D:/Python Programs/Neural Network"]).decode("utf8"))
+import numpy as np
+import pandas as pd 
 
 iris = pd.read_csv("D:/Python Programs/Neural Network/iris.csv")
 predict = pd.read_excel("D:/Python Programs/Neural Network/iris_predict.csv")
-# print(iris.head()) (prints first 5 rows and 5 columns)
-# print(iris.shape) (prints the shape of the .csv file)
-# print(iris.dtypes) (prints the data types of the features and labels)
 
 iris.iloc[:,0:3] = iris.iloc[:,0:3].astype(np.float32)
-iris["species"] = iris["species"].map({"Iris-setosa":0, "Iris-virginica":1, "Iris-versicolor":2}) # Conversion
+iris["species"] = iris["species"].map({"Iris-setosa":0, "Iris-virginica":1, "Iris-versicolor":2}) 
 
 from sklearn.model_selection import train_test_split 
-xtrain, xtest, ytrain, ytest = train_test_split(iris.iloc[:,0:4], iris["species"], test_size=0.33, random_state=42) #Split
+xtrain, xtest, ytrain, ytest = train_test_split(iris.iloc[:,0:4], iris["species"], test_size=0.33, random_state=42) 
 
 columns = iris.columns[0:4]
 
